5d654b9e39ac0a98b5ee8c50bb2c1ee84321c77f
Hubby <hubby@rmhubbert.com>
2025-01-28 22:39:35 +0000

    more keychron q8 configuration

---
+++ keyboards/keychron/q8/iso/keymaps/rmhubbert/config.h
@@ -0,0 +1,8 @@
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+#define PERMISSIVE_HOLD
+
+#define ENCODER_A_PINS { B12 }
+#define ENCODER_B_PINS { B13 }
