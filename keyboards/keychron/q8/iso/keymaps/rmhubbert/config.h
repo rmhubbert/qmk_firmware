22983fd229affcfb2cd57942d4f7906630c39e71
Hubby <hubby@rmhubbert.com>
2025-01-28 23:35:28 +0000

    more q8 configuration

--- keyboards/keychron/q8/iso/keymaps/rmhubbert/config.h
+++ keyboards/keychron/q8/iso/keymaps/rmhubbert/config.h
@@ -3,6 +3,3 @@
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
 #define PERMISSIVE_HOLD
-
-#define ENCODER_A_PINS { B12 }
-#define ENCODER_B_PINS { B13 }
