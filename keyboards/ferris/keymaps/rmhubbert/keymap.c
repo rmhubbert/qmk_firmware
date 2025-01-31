e69719e493098e90de93765f8d4af0f47f0f76e8
Hubby <hubby@rmhubbert.com>
2025-01-31 19:24:59 +0000

    add functions to handle home row mod rolls

--- keyboards/ferris/keymaps/rmhubbert/keymap.c
+++ keyboards/ferris/keymaps/rmhubbert/keymap.c
@@ -1,4 +1,8 @@
+#include <stdint.h>
+#include "action.h"
+#include "keycodes.h"
 #include QMK_KEYBOARD_H
+
 #if __has_include("keymap.h")
 #    include "keymap.h"
 #endif
@@ -15,15 +19,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /*movement layer*/
     [1] = LAYOUT_split_3x5_2(
-        KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,                                KC_NO, KC_ESC, KC_ENT, KC_TAB, KC_BSPC,
-        LALT_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_NO,          KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_QUOT,
+        KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,                                KC_NO, KC_NO, KC_ENT, KC_NO, KC_BSPC,
+        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_NO,          KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_QUOT,
         KC_NO, KC_NO, KC_CAPS, CW_TOGG, KC_NO,                                  KC_NO, KC_NO, KC_NO, KC_NO, KC_BSLS,
         KC_TRNS, KC_TRNS,                                                       KC_TRNS, KC_TRNS
     ),
 
     /*symbol layer*/
     [2] = LAYOUT_split_3x5_2(
-        KC_EXLM, KC_DQUO, KC_HASH, KC_DLR, KC_PERC,                             KC_CIRC, KC_AMPR, KC_ASTR, KC_P0, KC_NO,
+        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                               KC_CIRC, KC_AMPR, KC_ASTR, KC_P0, KC_NO,
         KC_MINS, KC_LCBR, KC_LBRC, KC_LPRN, KC_EQL,                             RALT(KC_3), RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN),
         KC_NUBS, KC_RCBR, KC_RBRC, KC_RPRN, KC_NO,                              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_TRNS, KC_TRNS,                                                       KC_TRNS, KC_TRNS
@@ -46,10 +50,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 // clang-format on
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LGUI_T(KC_A):
+            // disable accidental mod when rolling s & a
+            if (!disable_modifier_user(record, KC_LALT, KC_S, KC_A)) return false;
+            // disable accidental mod when rolling d & a
+            if (!disable_modifier_user(record, KC_LCTL, KC_D, KC_A)) return false;
+            // disable accidental mod when rolling f & a
+            if (!disable_modifier_user(record, KC_LSFT, KC_F, KC_A)) return false;
+        case LALT_T(KC_S):
+            // disable accidental mod when rolling a & s
+            if (!disable_modifier_user(record, KC_LGUI, KC_A, KC_S)) return false;
+            // disable accidental mod when rolling d & s
+            if (!disable_modifier_user(record, KC_LCTL, KC_D, KC_S)) return false;
+            // disable accidental mod when rolling f & s
+            if (!disable_modifier_user(record, KC_LGUI, KC_F, KC_S)) return false;
+        case LCTL_T(KC_D):
+            // disable accidental mod when rolling a & d
+            if (!disable_modifier_user(record, KC_LGUI, KC_A, KC_D)) return false;
+        case LSFT_T(KC_F):
+            // disable accidental mod when rolling a & f
+            return disable_modifier_user(record, KC_LGUI, KC_A, KC_F);
+    }
+    return true;
+};
 
-
+bool disable_modifier_user(keyrecord_t *record, uint16_t keycode, uint16_t first_tap, uint16_t second_tap) {
+    if (record->event.pressed && record->tap.count > 0) {
+        if (get_mods() & MOD_BIT(keycode)) {
+            unregister_mods(MOD_BIT(keycode));
+            tap_code(first_tap);
+            tap_code(second_tap);
+            add_mods(MOD_BIT(keycode));
+            return false;
+        }
+    }
+    return true;
+};
 
 #ifdef OTHER_KEYMAP_C
 #    include OTHER_KEYMAP_C
 #endif // OTHER_KEYMAP_C
-
---
+++ keyboards/ferris/keymaps/rmhubbert/keymap.h
@@ -0,0 +1,3 @@
+#include "action.h"
+
+bool disable_modifier_user(keyrecord_t *record, uint16_t keycode, uint16_t first_tap, uint16_t second_tap);
